7dad6b9c9cf3383aa9167e8d823d9cf390e8c6ae
pindee <emma.pindera@pointclickcare.com>
2024-04-03 11:28:18 -0400

    5

--- src/javascript.js
+++ src/javascript.js
@@ -1,48 +1,25 @@
-function alertTimezone(event) {
-  if (event.target.value == "paris") {
-    console.log(parisTime);
-  }
-  if (event.target.value == "tokyo") {
-    console.log(tokyoTime);
-  }
-  if (event.target.value == "sydney") {
-    console.log(sydneyTime);
-  }
+function updateTime() {
+  let selectedCity = document.querySelector("#selectedCity");
+  let CityTime = document.querySelector("#selecttime");
+  let CityDate = document.querySelector("#selectdate");
+
+  let updateClock = function () {
+    let currentTime = moment();
+    let city = citySelect.value;
+    let time = currentTime.tz(city).format("h:mm:ss A");
+    let date = currentTime.tz(city).format("dddd, MMMM D, YYYY");
+
+    selectedCity.innerHTML = city;
+    CityTime.innerHTML = time;
+    CityDate.innerHTML = date;
+  };
+
+  // Call updateClock immediately
+  updateClock();
+
+  // Call updateClock every second
+  setInterval(updateClock, 1000);
 }
 
-//Toronto Time
-let torontoTimeElement = document.querySelector("#totime");
-let torontoDateElement = document.querySelector("#todate");
-
-setInterval(function () {
-  let torontoTime = moment()
-    .tz("Canada/Toronto")
-    .format("h:mm:ss [<small>]A[</small>]");
-  let torontoDate = moment().tz("Canada/Toronto").format("dddd, MMMM D, YYYY");
-
-  torontoTimeElement.innerHTML = torontoTime;
-  torontoDateElement.innerHTML = torontoDate;
-}, 10);
-
-//Sydney Time
-let sydneyTimeElement = document.querySelector("#sydtime");
-let sydneyDateElement = document.querySelector("#syddate");
-
-setInterval(function () {
-  let sydneyTime = moment()
-    .tz("Australia/Sydney")
-    .format("h:m:ss [<small>]A[</small>]");
-  let sydneyDate = moment().tz("Australia/Sydney").format("dddd, MMMM D, YYYY");
-
-  sydneyTimeElement.innerHTML = sydneyTime;
-  sydneyDateElement.innerHTML = sydneyDate;
-}, 10);
-
-let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:m:ss A");
-
-let parisTime = moment()
-  .tz("Europe/Paris")
-  .format("dddd, MMMM D, YYYY h:m:ss A");
-
 let citySelect = document.querySelector("#citySelect");
-citySelect.addEventListener("change", alertTimezone);
+citySelect.addEventListener("change", updateTime);
