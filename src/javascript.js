418adb02dc415fc9034b3f9a14075b0f7892f22a
pindee <emma.pindera@pointclickcare.com>
2024-04-02 20:53:27 -0400

    Updated

--- src/javascript.js
+++ src/javascript.js
@@ -11,24 +11,38 @@ function alertTimezone(event) {
 }
 
 //Toronto Time
-let torontoTime = moment().tz("Canada/Toronto").format("h:mm A");
-let torontoDate = moment().tz("Canada/Toronto").format("dddd, MMMM D, YYYY");
 let torontoTimeElement = document.querySelector("#totime");
 let torontoDateElement = document.querySelector("#todate");
-torontoTimeElement.innerHTML = torontoTime;
-torontoDateElement.innerHTML = torontoDate;
+
+setInterval(function () {
+  let torontoTime = moment()
+    .tz("Canada/Toronto")
+    .format("h:mm:ss [<small>]A[</small>]");
+  let torontoDate = moment().tz("Canada/Toronto").format("dddd, MMMM D, YYYY");
+
+  torontoTimeElement.innerHTML = torontoTime;
+  torontoDateElement.innerHTML = torontoDate;
+}, 10);
 
 //Sydney Time
-let sydneyTime = moment().tz("Australia/Sydney").format("h:m A");
-let sydneyDate = moment().tz("Australia/Sydney").format("dddd, MMMM D, YYYY");
 let sydneyTimeElement = document.querySelector("#sydtime");
 let sydneyDateElement = document.querySelector("#syddate");
-sydneyTimeElement.innerHTML = sydneyTime;
-sydneyDateElement.innerHTML = sydneyDate;
 
-let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:m A");
+setInterval(function () {
+  let sydneyTime = moment()
+    .tz("Australia/Sydney")
+    .format("h:m:ss [<small>]A[</small>]");
+  let sydneyDate = moment().tz("Australia/Sydney").format("dddd, MMMM D, YYYY");
 
-let parisTime = moment().tz("Europe/Paris").format("dddd, MMMM D, YYYY h:m A");
+  sydneyTimeElement.innerHTML = sydneyTime;
+  sydneyDateElement.innerHTML = sydneyDate;
+}, 10);
+
+let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:m:ss A");
+
+let parisTime = moment()
+  .tz("Europe/Paris")
+  .format("dddd, MMMM D, YYYY h:m:ss A");
 
 let citySelect = document.querySelector("#citySelect");
 citySelect.addEventListener("change", alertTimezone);
