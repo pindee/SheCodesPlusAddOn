d9cbf85c89643104b483683a07a5be23db536393
pindee <emma.pindera@pointclickcare.com>
2024-04-02 20:33:22 -0400

    Ch2

--- src/javascript.js
+++ src/javascript.js
@@ -1,22 +1,34 @@
 function alertTimezone(event) {
   if (event.target.value == "paris") {
-    alert("It is " + parisTime + " in Paris, Europe 🥐");
+    console.log(parisTime);
   }
   if (event.target.value == "tokyo") {
-    alert("It is " + tokyoTime + " in Tokyo, Japan 🎌");
+    console.log(tokyoTime);
   }
   if (event.target.value == "sydney") {
-    alert("It is " + tokyoTime + " in Sydney, Australia 🐨");
+    console.log(sydneyTime);
   }
 }
+
+//Toronto Time
+let torontoTime = moment().tz("Canada/Toronto").format("h:mm A");
+let torontoDate = moment().tz("Canada/Toronto").format("dddd, MMMM D, YYYY");
+let torontoTimeElement = document.querySelector("#totime");
+let torontoDateElement = document.querySelector("#todate");
+torontoTimeElement.innerHTML = torontoTime;
+torontoDateElement.innerHTML = torontoDate;
+
+//Sydney Time
+let sydneyTime = moment().tz("Australia/Sydney").format("h:m A");
+let sydneyDate = moment().tz("Australia/Sydney").format("dddd, MMMM D, YYYY");
+let sydneyTimeElement = document.querySelector("#sydtime");
+let sydneyDateElement = document.querySelector("#syddate");
+sydneyTimeElement.innerHTML = sydneyTime;
+sydneyDateElement.innerHTML = sydneyDate;
 
 let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:m A");
 
 let parisTime = moment().tz("Europe/Paris").format("dddd, MMMM D, YYYY h:m A");
-
-let sydneyTime = moment()
-  .tz("Australia/Sydney")
-  .format("dddd, MMMM D, YYYY h:m A");
 
 let citySelect = document.querySelector("#citySelect");
 citySelect.addEventListener("change", alertTimezone);
